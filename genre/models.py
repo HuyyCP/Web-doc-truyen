26a624e685adce17a953b4bbd8dc91814a246874
huyycp <baohuyvongoc1@gmail.com>
2024-05-08 03:22:42 +0700

    fix genre and manga model

--- genre/models.py
+++ genre/models.py
@@ -1,4 +1,11 @@
+from typing import Any
 from django.db import models
+from django.utils.text import slugify
 
 class Genre(models.Model): 
-    genreName = models.CharField(max_length=50, null=False, blank=False)
+    genreName = models.CharField(max_length=50, unique=True, null=False, blank=False)
+    slug      = models.SlugField(max_length=60, default='')
+
+    def save(self):
+        self.slug = slugify(self.title)
+        super().save()
--- manga/models.py
+++ manga/models.py
@@ -2,10 +2,19 @@ from django.db import models
 from genre.models import Genre
 from author.models import Author
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 class Manga(models.Model):
     title       = models.CharField(max_length=100, null=False, blank=False)
+    slug        = models.SlugField(max_length=100, default='')
+    description = models.TextField(max_length=200, default='')
+    avatarLink  = models.CharField(max_length=200, default='')
     idDrive     = models.CharField(max_length=100, unique=True, null=False, blank=False)
     uploader    = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     author      = models.ForeignKey(Author, on_delete=models.DO_NOTHING)
     genres      = models.ManyToManyField(Genre)
+
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
